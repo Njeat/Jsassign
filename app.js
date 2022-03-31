2f915a257dded9d44e435b03aaecab79544d5825
parktj <ekswn120@gmail.com>
2022-03-31 20:37:39 +0900

    제목 내용 넘겨주기

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 var app = new function(){
     this.el = document.getElementById("tasks");
-    this.tasks=[];
+    this.title=[];
+    this.contents=[];
 
     this.FetchAll = function(){
         var data = '';
 
-        if(this.tasks.length > 0){
-            for(i = 0; i < this.tasks.length; i++){
-                data += '<tr>';
-                data += '<td>'+(i+1)+'. '+this.tasks[i]+'</td>';
-                data += '<td> <button onclick="app.Edit('+i+')" class="btn btn-warning">Edit</button></td> ';
-                data += '<td> <button onclick="app.Delete('+i+')" class="btn btn-danger">Delete</button></td> ';
-                data += '</tr>';
+        if(this.title.length > 0){
+            for(i = 0; i < this.title.length; i++){
+                // data += '<div class="card">';
+                data += '<div class="card-body">'
+                data += '<h5 class="card-title">'+(i+1)+'. '+this.title[i]+'</h5>';
+                data += '<p>' + this.contents[i] + '</p>';
+                data += '<button onclick="app.Edit('+i+')" class="btn btn-warning">Edit</button>';
+                data += '<button onclick="app.Delete('+i+')" class="btn btn-danger">Delete</button>';
+                data += '</div>';
+                // data += '</tr>';
             }
         }
 
@@ -19,25 +23,29 @@ var app = new function(){
     };
 
     this.Add = function(){
-        el = document.getElementById('add-memo')
-        var task = el.value;
-        if (task) {
-            this.tasks.push(task.trim());   // trim() 함수는 양옆 공백 없애기
-            el.value = '';
+        el1 = document.getElementById('add-memo')
+        el2 = document.getElementById('add-word')
+        var task1 = el1.value;
+        var task2 = el2.value;
+        if (task1) {
+            this.title.push(task1.trim());   // trim() 함수는 양옆 공백 없애기
+            this.contents.push(task2.trim());
+            el1.value = '';
+            el2.value = '';
             this.FetchAll();
         }
     };
     
     this.Edit = function(item){
         var el = document.getElementById('edit-todo');
-        el.value = this.tasks[item];
+        el.value = this.title[item];
         document.getElementById('edit-box').style.display = 'block';
         self = this;
 
         document.getElementById('save-edit').onsubmit = function() {
             var task = el.value;
             if(task){
-                self.tasks.splice(item, 1, task.trim());
+                self.title.splice(item, 1, task.trim());
                 self.FetchAll();
                 CloseInput();
             }
@@ -45,7 +53,7 @@ var app = new function(){
     };
 
     this.Delete = function(item){
-        this.tasks.splice(item, 1)
+        this.title.splice(item, 1)
         this.FetchAll()
 
     };
